e3ef975753754b11846c7b6ac02115be5bb45612
SzAkos98 <36414715+SzAkos98@users.noreply.github.com>
2018-09-17 08:14:55 +0200

    Add files via upload

---
+++ yoda.java
@@ -0,0 +1,17 @@
+class yoda {
+
+
+ public static void main (String[] args){
+	String S = null;
+		try{
+		if(S.equals("szo")){
+		}
+		} catch (NullPointerException e){
+		System.out.println("Nem követi a Xoda conditions-t");
+		System.exit(0);
+		}
+		}
+	}
+
+
+
