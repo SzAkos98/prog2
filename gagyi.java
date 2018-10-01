95e2bfb0ad54e03e6ecd07fee97dada7f4e166bf
SzAkos98 <36414715+SzAkos98@users.noreply.github.com>
2018-10-01 08:46:31 +0200

    Add files via upload

---
+++ gagyi.java
@@ -0,0 +1,19 @@
+package gagyi;
+
+public class gagyi {
+	public static void main (String[] args) {
+		Integer x = 127;
+		Integer t = 127;
+		
+		while ( x<=t && x>=t && x!=t) {
+			System.out.println("Ennek nem kellene lefutni.");
+		}
+		x = 128;
+		t = 128;
+		while ( x<=t && x>=t && x!=t) {
+			System.out.println("Ennek le kell futni");
+		}
+	}
+	
+
+}
